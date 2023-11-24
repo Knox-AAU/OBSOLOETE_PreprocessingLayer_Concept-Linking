3e5dc36258639b6d1c5157fa90f54bd537f5e704
Vi Thien Le <YP02DI@student.aau.dk>
2023-11-24 15:33:02 +0100

    CleanupMain

--- program/main.py
+++ program/main.py
@@ -33,24 +33,4 @@ def stringComparisonSolution():
     triples = generateTriples(data, ontTypes)
     # Convert the array to a JSON string
     writeFile("../files/output.json", json.dumps(triples))
-
-
-g = Graph()
-g.parse("files/ontology.ttl", format="ttl")
-
-# TODO: Get all datatype properties from ontology.
-query = '''
-PREFIX dbo: <http://dbpedia.org/ontology/>
-
-SELECT DISTINCT ?exampleValue
-WHERE {
-  ?resource dbo:area ?exampleValue .
-  FILTER(isLiteral(?exampleValue))
-}
-LIMIT 10
-'''
-result = g.query(query)
-for row in result:
-    print(row)
-        
 #stringComparisonSolution()
