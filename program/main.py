da8de3bfaed09bf1153d2c27e088457e0a4a07ca
Vi Thien Le <YP02DI@student.aau.dk>
2023-11-24 15:29:45 +0100

    [FIX] untrainedSpacySolution

--- program/main.py
+++ program/main.py
@@ -1,8 +1,8 @@
 from functions.ontology import *
-from functions.spacy import *
+from functions.untrainedSpacy import *
 import json
 
-f = open("../files/test.JSON")
+f = open("../files/entity_mentions.json",  encoding="utf-8")
 data = json.load(f)
 
 def generateTXTfiles():
@@ -18,9 +18,15 @@ def generateTXTfiles():
 
 def untrainedSpacySolution():
     labelsDict = linkSpacyLabels()
-    triples = createMagicUnfinished(labelsDict, data)
-    print(*triples, sep="\n")
-#untrainedSpacySolution()
+    triples = generateTriplesFromJSON(labelsDict, data)
+    
+    output_path = "../files/output.json"
+    with open(output_path, "w", encoding = "utf-8") as outfile:
+        json.dump(triples, outfile, ensure_ascii=False, indent=4)
+
+    #writeFile("../files/output.json", json.dumps(triples))
+
+untrainedSpacySolution()
 
 def stringComparisonSolution():
     ontTypes = queryLabels()
@@ -28,6 +34,23 @@ def stringComparisonSolution():
     # Convert the array to a JSON string
     writeFile("../files/output.json", json.dumps(triples))
 
-    
+
+g = Graph()
+g.parse("files/ontology.ttl", format="ttl")
+
+# TODO: Get all datatype properties from ontology.
+query = '''
+PREFIX dbo: <http://dbpedia.org/ontology/>
+
+SELECT DISTINCT ?exampleValue
+WHERE {
+  ?resource dbo:area ?exampleValue .
+  FILTER(isLiteral(?exampleValue))
+}
+LIMIT 10
+'''
+result = g.query(query)
+for row in result:
+    print(row)
         
-stringComparisonSolution()
+#stringComparisonSolution()
