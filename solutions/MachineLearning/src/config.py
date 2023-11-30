bf39574f4951f1f93fb5b240494e2ad1cfcc1960
denBruneBarone <runeberhardt@gmail.com>
2023-11-30 10:39:50 +0100

    ML solution

---
+++ solutions/MachineLearning/src/config.py
@@ -0,0 +1,11 @@
+class ModelConfig:
+    input_size = 10000
+    embedding_dim = 100
+    hidden_size = 128
+    num_classes = 10
+
+
+class TrainingConfig:
+    num_epochs = 10
+    batch_size = 32
+    learning_rate = 0.001
