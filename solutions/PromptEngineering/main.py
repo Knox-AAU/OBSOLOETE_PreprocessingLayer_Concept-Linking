6683eab5c7ff2ec4d7dc28f1bb9dc27994f164a9
casparejensen <casparejensen@gmail.com>
2023-12-03 01:52:02 +0100

    Fixed Llama

--- solutions/PromptEngineering/main.py
+++ solutions/PromptEngineering/main.py
@@ -1,19 +1,22 @@
 import json
 import re
 import time
+import requests
 
-from llama_cpp import Llama
+api_url = "http://127.0.0.1:5000/llama"
+headers = {"Content-Type": "application/json"}
 
-LLM = Llama(model_path="C:/Users/lucas/GitRepos/Llama_2/llama.cpp/models/7B/ggml-model-q4_0.bin", n_ctx=32768)
 
 # Read all ontology classes from file
 def read_ontology_class():
-    with open('../files/person.txt') as file:
-        return [line.strip() for line in file]
+    with open('../../data/files/PromptEngineering/person.txt') as file1:
+        return [line.strip() for line in file1]
+
 
 # Open the JSON file with sentences for reading - use as input
-with open('../files/sentence.json', 'r') as file:
-    data = json.load(file)
+with open('../../data/files/PromptEngineering/sentence.json', 'r') as file2:
+    data = json.load(file2)
+
 
 # Generates the respective triples of the right format
 def generate_triples(output_data):
@@ -22,6 +25,7 @@ def generate_triples(output_data):
         for mention in sentence_data["entityMentions"]:
             iri = mention["iri"]
             classification = mention["classification"]
+            print(f'GenerateTriples() - Entity: {mention}, Classification: {classification}')
 
             # Skip creating triples if the classification is "Unknown"
             if classification == "Unknown":
@@ -36,133 +40,123 @@ def generate_triples(output_data):
     return triples
 
 
-ontology_classes = "Person, Place, Time, Organisation, Event, Academic, AcademicConference"
+def classify_entity_mentions():
+    # Needs to be replaced with ontology
+    ontology_classes = "Person, Place, Time, Organisation, Event, Academic, AcademicConference"
+    # creates a list of ontology_classes
+    ontology_classes_list = [cls.strip() for cls in ontology_classes.split(',')]
+    output_data = {"sentences": []}
+    max_retries = 3
 
-output_data = {"sentences": []}
+    # Iterate through each sentence
+    for sentence_data in data[0]["sentences"]:
+        content_sentence = sentence_data["sentence"]
 
-max_retries = 3
+        # Iterate through each entity mention in the sentence
+        for mention in sentence_data["entityMentions"]:
+            if mention["type"] == "Entity":
+                content_entity = mention["name"]
+                print(f'Entity to classify: {content_entity}')
 
-# Iterate through each sentence
-for sentence_data in data[0]["sentences"]:
-    content_sentence = sentence_data["sentence"]
+                prompt_template = {
+                    "system_message": ("The input sentence is all your knowledge. \n"
+                                       "Do not answer if it can't be found in the sentence. \n"
+                                       "Do not use bullet points. \n"
+                                       "Do not identify entity mentions yourself, use the provided ones \n"
+                                       "Given the input in the form of the content from a file: \n"
+                                       "[Sentence]: {content_sentence} \n"
+                                       "[EntityMention]: {content_entity} \n"),
 
-    # Iterate through each entity mention in the sentence
-    for mention in sentence_data["entityMentions"]:
-        if mention["type"] == "Entity":
-            content_entity = mention["name"]
+                    "user_message": ("Classify the [EntityMention] in regards to ontology classes: {ontology_classes} \n"
+                                     "The output answer must be in JSON in the following format: \n"
+                                     "{{ \n"
+                                     "'Entity': 'Eiffel Tower', \n"
+                                     "'Class': 'ArchitecturalStructure' \n"
+                                     "}} \n"),
 
-            prompt_template = (
-                "##Context: \n"
-                "The input sentence is all your knowledge. \n"
-                "Do not answer if it can't be found in the sentence. \n"
-                "Do not use bullet points. \n"
-                "Given the input in the form of the content from a file: \n"
-                "[Sentence]: {content_sentence} \n"
-                "[EntityMention]: {content_entity} \n"
-                "##Prompt: \n"
-                f"Classify the entity in regards to ontology classes: {read_ontology_class()} \n"
-                "The output answer must be in JSON in the following format: \n"
-                "{{ \n"
-                "'Entity': 'Eiffel Tower', \n"
-                "'Class': 'ArchitecturalStructure' \n"
-                "}} \n"
-            )
+                    "max_tokens": 4092
+                }
 
-            prompt = prompt_template.format(
-                content_sentence=content_sentence,
-                content_entity=content_entity,
-                ontology_classes=ontology_classes,
-            )
+                outer_while_retry_count = 0
+                while True:  # Run until entity is mapped to a provided ontology class
+                    outer_while_retry_count += 1
+                    print(f'--- RUN Count #{outer_while_retry_count} (Outer While loop) ---')
+                    prompt = {key: value.format(
+                        content_sentence=content_sentence,
+                        content_entity=content_entity,
+                        ontology_classes=ontology_classes
+                    ) if isinstance(value, str) else value for key, value in prompt_template.items()}
 
-            print(prompt)
+                    inner_while_retry_count = 0
+                    result_text = ''
+                    # Retrying until we get a not none response
+                    while inner_while_retry_count < max_retries:
+                        inner_while_retry_count += 1
+                        print(f'--- RUN Count #{inner_while_retry_count} (Inner While loop) ---')
+                        response = requests.post(api_url, data=json.dumps(prompt), headers=headers)
 
-            retry_count = 0
-            while retry_count < max_retries:
-                output = LLM(prompt, max_tokens=10000)
+                        if response.status_code == 200:
+                            output = response.json()  # Assuming the response is in JSON format
 
-                if output["choices"] and output["choices"][0]["text"] not in (None, '.'):
-                    result_text = output["choices"][0]["text"]
-                    break
-                else:
-                    print("Output is null, empty, or contains only a dot. Retrying...")
-                    retry_count += 1
-                    time.sleep(2)
+                            if output["choices"] and output["choices"][0]["text"] not in (None, '.'):
+                                result_text = output["choices"][0]["text"]
+                                break
+                            else:
+                                print("Output is null, empty, or contains only a dot. Retrying...")
+                                time.sleep(2)
+                        else:
+                            print(f"Error: {response.status_code} - {response.text}")
 
-            result_text = result_text.strip()
+                    # Find the index of [/INST]
+                    inst_index = result_text.find('[/INST]')
 
-            match = re.search(r"'Class': ['\"](\w+)['\"]", result_text)
-            classification = match.group(1) if match else "Unknown"
+                    # Extract text after [/INST]
+                    if inst_index != -1:
+                        inst_text = result_text[inst_index + len('[/INST]'):]
+                        match = re.search(r"'Class': ['\"](\w+)['\"]", inst_text)
+                        classification = match.group(1) if match and match.group(1) in ontology_classes_list else None
 
-            output_sentence_data = {
-                "sentence": content_sentence,
-                "entityMentions": [
-                    {
-                        "name": mention["name"],
-                        "startIndex": mention["startIndex"],
-                        "endIndex": mention["endIndex"],
-                        "iri": mention["iri"],
-                        "classification": classification
-                    }
-                ]
-            }
+                        if classification:
+                            print(f'Classification result: {classification}')
+                            output_sentence_data = {
+                                "sentence": content_sentence,
+                                "entityMentions": [
+                                    {
+                                        "name": mention["name"],
+                                        "startIndex": mention["startIndex"],
+                                        "endIndex": mention["endIndex"],
+                                        "iri": mention["iri"],
+                                        "classification": classification
+                                    }
+                                ]
+                            }
+                            output_data["sentences"].append(output_sentence_data)
+                            break  # Exit the while loop if entity is mapped to a provided ontology class
 
-            output_data["sentences"].append(output_sentence_data)
-
-            # Print the result for the current sentence
-            print("Sentence:", content_sentence)
-            print("Entity Mention:", content_entity)
-            print("Classification:", classification)
-            print(output["choices"][0]["text"])
-
-# Specify the output file path
-output_file_path = "output.json"
-
-# Generate triples
-triples = generate_triples(output_data)
-
-# Specify the triples output file path
-triples_file_path = "Triples.json"
-
-# Save the triples to a JSON file
-with open(triples_file_path, 'w') as triples_file:
-    json.dump(triples, triples_file, indent=2)
-
-# Save the result to a JSON file
-with open(output_file_path, 'w') as output_file:
-    json.dump(output_data, output_file, indent=2)
-
-print("Output saved to:", output_file_path)
-
-print("Triples saved to:", triples_file_path)
+    return output_data
 
 
-# f"Given the input in the form of the content from the file: {content}. \n"
-# f"Classify the entities in regards to ontology classes: {read_ontology_class()} \n"
-# f"The output answer should only be the name of the class matching the most. \n"
-# f"Create a list of each entity mention and its classification. \n"
-# f"The output must be in JSON format"
+if __name__ == '__main__':
+    # Specify the output file path
+    output_file_path = '../../data/files/PromptEngineering/output.json'
 
+    # Classify entity mentions using llama api
+    output_data_response = classify_entity_mentions()
 
-# f"##Context: \n"
-# f"The data is all your knowledge. \n"
-# f"Do not answer, if it can't be found in the data. \n"
-# f"If so, respond in one sentence 'I can't answer that question'. \n"
-# f"Do not use bullet points \n"
-# f"##Prompt format: \n"
-# f"data: \n"
-# f"The given input: \n"
-# f"For each [Sentence] in Sentences: {content_sentence} \n"
-# f"And for each [EntityMention]: {content_entity}. \n"
-# f"The ontology classes: {ontology_classes}. \n"
-# f"Question: \n"
-# f"Please classify the entity mentions from the sentences, \n"
-# f"based on the ontology classes (Answer this based on data)."
+    # Generate triples
+    generated_triples = generate_triples(output_data_response)
 
+    # Specify the triples output file path
+    triples_file_path = '../../data/files/PromptEngineering/triples.json'
 
-# f"Given the input in the form of the content from the file: \n"
-# f"[Sentences]: {content_sentence} \n"
-# f"[EntityMention]: {content_entityMentions} \n"
-# f"Classify the entities in regards to ontology classes: {ontology_classes} \n"
-# f"The output answer should only be the name of the class matching the most. \n"
-# f"Create a list of the used classes for each entity mention and its classification. \n"
-# f"The output must be in JSON format"
+    # Save the triples to a JSON file
+    with open(triples_file_path, 'w') as triples_file:
+        json.dump(generated_triples, triples_file, indent=2)
+
+    # Save the result to a JSON file
+    with open(output_file_path, 'w') as output_file:
+        json.dump(output_data_response, output_file, indent=2)
+
+    print("Output saved to:", output_file_path)
+
+    print("Triples saved to:", triples_file_path)
